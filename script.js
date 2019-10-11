3b0dd22d558fb1493736859e1a7d8989ebbd7167
David Buckley <davidjaybuckley@gmail.com>
2019-10-11 09:57:03 -0600

    generates butter

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-$(document)
-    .ready(function() {
+console.log('js runs')
 
-      // fix menu when passed
-      $('.masthead')
-        .visibility({
-          once: false,
-          onBottomPassed: function() {
-            $('.fixed.menu').transition('fade in');
-          },
-          onBottomPassedReverse: function() {
-            $('.fixed.menu').transition('fade out');
-          }
-        })
-      ;
-
-      // create sidebar and attach to menu open
-      $('.ui.sidebar')
-        .sidebar('attach events', '.toc.item')
-      ;
-
-    })
-  ;
+$.getJSON( "object.json", function( json ) {
+  console.log(json);
+  for (let i = 0; i < json.length; i++) {
+    $("#butter-container").append(`
+        <div class="adaptable squishable half row container">
+          <div class="two thirds container">
+            <div class="small left heading">${json[i].name}</div>
+            <div class="left text">${json[i].discription}</div>
+            <div class="left text">${json[i].price}</div>
+          </div>
+          <div class="third vertically centered container">
+            <img class="full image" src="${json[i].imgSrc}">
+          </div>
+        </div>
+        `)
+  }
+})
